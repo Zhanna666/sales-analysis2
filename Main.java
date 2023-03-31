98c8522c702a179ae124f91cd9f343d5a4c38719
Zhanna666 <snezhana_perepelitsa@mail.ru>
2023-03-31 19:55:55 +0300

    Adden longer

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int[] sales = {5, 10, 7, 9, 12, 18, 6, 2};
-
-        SalesManager salesManager = new SalesManager(sales);
-        System.out.println(salesManager.max());
-    }
-}
